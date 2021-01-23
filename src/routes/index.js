466fd627b3108cc840c24a8f0f2813657abc7b51
Chris <dat.pham.a4@gmail.com>
2021-01-23 12:34:47 +0700

    fix index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,17 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-// /* GET home page. */
-// // router.get('/', function(req, res, next) {
-// //   res.render('index', { title: 'Express' });
-// //   console.log(1);
-// // });
-
-
-// router.get('/', (req, res, next) => {
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
 //   console.log(1);
-//   res.sendFile('iot/routes/wget-log.txt');
 // });
 
 
-// module.exports = router;
+router.get('/', (req, res, next) => {
+  console.log(1);
+  res.sendFile(__dirname + '/wget-log.txt');
+});
+
+
+module.exports = router;
