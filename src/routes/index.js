6e2e9bb49f9f60165fe18dfdbc1207ef838b6659
Chris <dat.pham.a4@gmail.com>
2021-01-23 12:29:13 +0700

    del index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
+// /* GET home page. */
+// // router.get('/', function(req, res, next) {
+// //   res.render('index', { title: 'Express' });
+// //   console.log(1);
+// // });
+
+
+// router.get('/', (req, res, next) => {
 //   console.log(1);
+//   res.sendFile('iot/routes/wget-log.txt');
 // });
 
 
-router.get('/', (req, res, next) => {
-  console.log(1);
-  res.sendFile('/home/rogers/IOT-server/routes/wget-log.txt');
-});
-
-
-module.exports = router;
+// module.exports = router;
